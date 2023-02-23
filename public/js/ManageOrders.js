e40a5eaa2843641133a065ee1aabcd0017a29940
JJ38 <jamesbrass@ymail.com>
2023-02-23 19:44:19 +0000

    can now print multiple orders at once

--- public/js/ManageOrders.js
+++ public/js/ManageOrders.js
@@ -1,6 +1,6 @@
 
+const orderElements = document.querySelectorAll('tr');
 const printButtons = document.querySelectorAll('.print');
-const orderData = document.querySelectorAll('tr');
 const selectAllButton = document.getElementById('selectall');
 const deletedSelectedButton = document.getElementById('deleteselected');
 const printSelectedButton = document.getElementById('printselected');
@@ -9,7 +9,7 @@ const allCheckBoxes = document.querySelectorAll('input[type=checkbox]');
 
 for(let i = 0; i < printButtons.length; i++){
     printButtons[i].addEventListener('click', e => {
-        printPage(i + 1);
+        printPage(orderElements[i + 1].children[1].innerHTML);
     });
 }
 
@@ -44,165 +44,217 @@ function printSelected(){
     orderIDs.push(selectedCheckBoxes[i].value);
   }
 
-  console.log(orderIDs);
+  print(generateForms(orderIDs));
+
 
 }
 
 
 function closePrint() {
-    document.body.removeChild(this.__container__);
+  document.body.removeChild(this.__container__);
+}
+
+function setPrint() {
+  this.contentWindow.__container__ = this;
+  this.contentWindow.onbeforeunload = closePrint;
+  this.contentWindow.onafterprint = closePrint;
+  this.contentWindow.focus(); // Required for IE
+  this.contentWindow.print();
+}
+
+function printPage(orderNumber) {
+
+  const form = generateForms([orderNumber]);
+  print(form);
+
+}
+
+
+function print(form){
+
+  const hideFrame = document.createElement("iframe");
+  hideFrame.onload = setPrint;
+  hideFrame.style.position = "fixed";
+  hideFrame.style.right = "0";
+  hideFrame.style.bottom = "0";
+  hideFrame.style.width = "0";
+  hideFrame.style.height = "0";
+  hideFrame.style.border = "0";
+
+  hideFrame.srcdoc = form;
+  document.body.appendChild(hideFrame);
+}
+
+function generateForms(orderIDs){
+
+  let html;
+
+  //const tableData = orderData[orderNumber].getElementsByTagName('td');
+
+
+
+  let boilerplateTop = '<!DOCTYPE html>'+
+    '<html lang="en">'+
+      '<head>'+
+        '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">'+
+        '<meta name="viewport" content="width=device-width, initial-scale=1.0">'+
+        '<script src="https://kit.fontawesome.com/dce6efa4ea.js" crossorigin="anonymous"></script>'+
+        '<link rel="stylesheet" href="css/PrintableForm.css" type="text/css">'+
+        ''+
+      '</head>'+
+        '<body>';
+         
+        
+
+  let boilerplateBottom = '</body>'+  
+  '</html>';
+
+  
+
+  html = boilerplateTop;
+
+  console.log(orderIDs[0]);
+
+  console.log(orderElements[1].children[1].innerHTML);
+
+  orderData = [];
+
+  for(let i = 1; i < orderElements.length; i++){
+    if(orderIDs.includes(orderElements[i].children[1].innerHTML)){
+      orderData.push(orderElements[i]);
+    }
   }
 
-  function setPrint() {
-    this.contentWindow.__container__ = this;
-    this.contentWindow.onbeforeunload = closePrint;
-    this.contentWindow.onafterprint = closePrint;
-    this.contentWindow.focus(); // Required for IE
-    this.contentWindow.print();
+  console.log(orderData);
+
+  for(let i = 0; i < orderIDs.length; i++){
+    const orderHTML = getOrderHTML(orderData[i].getElementsByTagName('td'));
+    html = html + orderHTML;
   }
 
-  async function printPage(orderNumber) {
-    const hideFrame = document.createElement("iframe");
-    hideFrame.onload = setPrint;
-    hideFrame.style.position = "fixed";
-    hideFrame.style.right = "0";
-    hideFrame.style.bottom = "0";
-    hideFrame.style.width = "0";
-    hideFrame.style.height = "0";
-    hideFrame.style.border = "0";
-    const form =  await generateForm(orderNumber);
-    //hideFrame.src = "../private/app/templates/PrintableForm.html";
-    hideFrame.srcdoc = form;
-    document.body.appendChild(hideFrame);
-  }
+  html = html + boilerplateBottom;
+  
+  console.log(html);
 
-  async function generateForm(orderNumber){
-
-    const tableData = orderData[orderNumber].getElementsByTagName('td');
-    console.log(tableData);
-
-    let form = '<!DOCTYPE html>'+
-                '<html lang="en">'+
-                '<head>'+
-                  '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">'+
-                  '<meta name="viewport" content="width=device-width, initial-scale=1.0">'+
-                  '<script src="https://kit.fontawesome.com/dce6efa4ea.js" crossorigin="anonymous"></script>'+
-                  '<link rel="stylesheet" href="css/PrintableForm.css" type="text/css">'+
-                  ''+
-                '</head>'+
-                  '<body>'+
-                  '<div class="page">'+
-                    '<h1>'+
-                      'High Flyers'+
-                    '</h1>'+
-                    '<h2>'+
-                      'Telephone: 07887 781089'+
-                    '</h2>'+
-                      '<div class="formwrapper">'+
-                        '<div class="wrapper">'+
-                          '<h3>'+
-                            'Pick up from:'+
-                          '</h3>'+
-                          '<div class="infowrapper">'+
-                            '<label>Name:</label>'+
-                            '<p>' + tableData[4].innerHTML + '</p>'+
-                  
-
-                            '<label>Address:</label>'+
-                            '<address>' + 
-                              '<p>' + tableData[5].innerHTML + '</p>'+
-                              '<p>' + tableData[6].innerHTML + '</p>'+
-                              '<p>' + tableData[7].innerHTML + '</p>'+
-                              '<p>' + tableData[8].innerHTML + '</p>'+
-                            '</address>' +
-                        
-                        
-                          '<label>Telephone:</label>'+
-                          '<p>' + tableData[9].innerHTML + '</p>'+
-                          
-                        
-                          '</div>'+
-                        '</div>'+
-                        '<div class="wrapper">'+
-                          '<h3>'+
-                           'Deliver to:'+
-                          '</h3>'+
-                            '<div class="infowrapper">'+
-                              '<label>Name:</label>'+
-                              '<p>' + tableData[10].innerHTML + '</p>'+
-                      
-                      
-                              '<label>Address:</label>'+
-                              '<address>' + 
-                                '<p>' + tableData[11].innerHTML + '</p>'+
-                                '<p>' + tableData[12].innerHTML + '</p>'+
-                                '<p>' + tableData[13].innerHTML + '</p>'+
-                                '<p>' + tableData[14].innerHTML + '</p>'+
-                              '</address>' +
-                                            
-                              '<label>Telephone:</label>'+
-                              '<p>' + tableData[15].innerHTML + '</p>'+
-                            '</div>'+
-                        '</div>'+
-                    '</div>'+
-                  '</div>'+
-                  '<div class="page">'+
-                    '<h1>'+
-                      'High Flyers'+
-                    '</h1>'+
-                    '<h2>'+
-                      'Telephone: 07887 781089'+
-                    '</h2>'+
-                    '<div class="formwrapper">'+
-                      '<div class="wrapper">'+
-                        '<h3>'+
-                          'Pick up from:'+
-                        '</h3>'+
-                        '<div class="infowrapper">'+
-                          '<label>Name:</label>'+
-                          '<p>' + tableData[4].innerHTML +'</p>'+
-                  
-
-                          '<label>Address:</label>'+
-                          '<address>' + 
-                            '<p>' + tableData[5].innerHTML + '</p>'+
-                            '<p>' + tableData[6].innerHTML + '</p>'+
-                            '<p>' + tableData[7].innerHTML + '</p>'+
-                            '<p>' + tableData[8].innerHTML + '</p>'+
-                          '</address>' +
-                    
-                    
-                          '<label>Telephone:</label>'+
-                          '<p>' + tableData[9].innerHTML + '</p>'+
-                      
-                        '</div>'+ 
-                      '</div>'+
-                      '<div class="wrapper">'+
-                        '<h3>'+
-                        'Deliver to:'+
-                        '</h3>'+
-                        '<div class="infowrapper">'+
-                          '<label>Name:</label>'+
-                          '<p>' + tableData[10].innerHTML + ' </p>'+
-                      
-                      
-                          '<label>Address:</label>'+
-                          '<address>' + 
-                            '<p>' + tableData[11].innerHTML + '</p>'+
-                            '<p>' + tableData[12].innerHTML + '</p>'+
-                            '<p>' + tableData[13].innerHTML + '</p>'+
-                            '<p>' + tableData[14].innerHTML + '</p>'+
-                          '</address>' +
-                                        
-                          '<label>Telephone:</label>'+
-                          '<p>' + tableData[15].innerHTML + '</p>'+
-                        '</div>'+
-                      '</div>'+
-                  '</div>'+
-                '</div>'+
-              ''+
-              '</body>'+
-            '</html>'
-      return form;
+  return html;
     
-  }
+}
+
+function getOrderHTML(tableData){
+
+  console.log(tableData[5].innerHTML);
+
+  let order =  
+  '<div class="page">'+
+    '<h1>'+
+      'High Flyers'+
+    '</h1>'+
+    '<h2>'+
+      'Telephone: 07887 781089'+
+    '</h2>'+
+      '<div class="formwrapper">'+
+        '<div class="wrapper">'+
+          '<h3>'+
+            'Pick up from:'+
+          '</h3>'+
+          '<div class="infowrapper">'+
+            '<label>Name:</label>'+
+            '<p>' + tableData[5].innerHTML + '</p>'+
+  
+
+            '<label>Address:</label>'+
+            '<address>' + 
+              '<p>' + tableData[6].innerHTML + '</p>'+
+              '<p>' + tableData[7].innerHTML + '</p>'+
+              '<p>' + tableData[8].innerHTML + '</p>'+
+              '<p>' + tableData[9].innerHTML + '</p>'+
+            '</address>' +
+        
+        
+          '<label>Telephone:</label>'+
+          '<p>' + tableData[10].innerHTML + '</p>'+
+          
+        
+          '</div>'+
+        '</div>'+
+        '<div class="wrapper">'+
+          '<h3>'+
+           'Deliver to:'+
+          '</h3>'+
+            '<div class="infowrapper">'+
+              '<label>Name:</label>'+
+              '<p>' + tableData[11].innerHTML + '</p>'+
+      
+      
+              '<label>Address:</label>'+
+              '<address>' + 
+                '<p>' + tableData[12].innerHTML + '</p>'+
+                '<p>' + tableData[13].innerHTML + '</p>'+
+                '<p>' + tableData[14].innerHTML + '</p>'+
+                '<p>' + tableData[15].innerHTML + '</p>'+
+              '</address>' +
+                            
+              '<label>Telephone:</label>'+
+              '<p>' + tableData[16].innerHTML + '</p>'+
+          '</div>'+
+        '</div>'+
+      '</div>'+
+    '</div>'+
+  '<div class="page">'+
+    '<h1>'+
+      'High Flyers'+
+    '</h1>'+
+    '<h2>'+
+      'Telephone: 07887 781089'+
+    '</h2>'+
+    '<div class="formwrapper">'+
+      '<div class="wrapper">'+
+        '<h3>'+
+          'Pick up from:'+
+        '</h3>'+
+        '<div class="infowrapper">'+
+          '<label>Name:</label>'+
+          '<p>' + tableData[5].innerHTML +'</p>'+
+  
+
+          '<label>Address:</label>'+
+          '<address>' + 
+            '<p>' + tableData[6].innerHTML + '</p>'+
+            '<p>' + tableData[7].innerHTML + '</p>'+
+            '<p>' + tableData[8].innerHTML + '</p>'+
+            '<p>' + tableData[9].innerHTML + '</p>'+
+          '</address>' +
+    
+    
+          '<label>Telephone:</label>'+
+          '<p>' + tableData[9].innerHTML + '</p>'+
+      
+        '</div>'+ 
+      '</div>'+
+      '<div class="wrapper">'+
+        '<h3>'+
+        'Deliver to:'+
+        '</h3>'+
+        '<div class="infowrapper">'+
+          '<label>Name:</label>'+
+          '<p>' + tableData[11].innerHTML + ' </p>'+
+      
+      
+          '<label>Address:</label>'+
+          '<address>' + 
+            '<p>' + tableData[12].innerHTML + '</p>'+
+            '<p>' + tableData[13].innerHTML + '</p>'+
+            '<p>' + tableData[14].innerHTML + '</p>'+
+            '<p>' + tableData[15].innerHTML + '</p>'+
+          '</address>' +
+                        
+          '<label>Telephone:</label>'+
+          '<p>' + tableData[16].innerHTML + '</p>'+
+        '</div>'+
+      '</div>'+
+  '</div>'+
+'</div>'    
+
+return order;
+
+}
