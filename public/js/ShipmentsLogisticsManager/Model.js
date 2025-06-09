95f0f573497628bead7aa22e1bd38989fdfe67d6
JJ38 <jamesbrass@ymail.com>
2025-06-09 22:51:37 +0100

    stops that have coordinates set to null are added to the unassigned stops run even if they have a valid postcode

--- public/js/ShipmentsLogisticsManager/Model.js
+++ public/js/ShipmentsLogisticsManager/Model.js
@@ -269,6 +269,10 @@ function assignStop(stop, stopPostcode, runDocuments, runDefinitions){
 
   }
 
+  if(stop.coordinates == null){
+    runName = null;
+  }
+
   const run = runDocuments.find((run) => {
     return run.runName === runName;
   })
