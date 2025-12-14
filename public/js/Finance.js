0eca3475dece75aab0a8a52e90169a8381c4e255
JJ38 <jamesbrass@ymail.com>
2025-12-14 18:40:27 +0000

    added editing capabilities of metadata to invoice preview

--- public/js/Finance.js
+++ public/js/Finance.js
@@ -1,10 +1,20 @@
 // import jsPDF from "jspdf";
 // import html2canvas from "html2canvas";
-
 import { jsPDF } from "jspdf";
 
-
 const generateInvoiceButton = document.getElementById('generate_invoice_button');
+
+const menu = document.getElementById('floating_options_menu');
+const deleteOptionsMenuButton = document.getElementById('delete_options_menu_button');
+const addOptionsMenuButton = document.getElementById('add_options_menu_button');
+const selectableElementGroups = document.querySelectorAll('.metadataGroup');
+const selectableElements = [...document.querySelectorAll('[contenteditable]')];
+
+console.log(selectableElements);
+
+let selectedElement;
+let isItemSelected;
+
 
 // Default export is a4 paper, portrait, using millimeters for units
 
@@ -21,25 +31,176 @@ function addEventListeners(){
         });
     }
 
+    if(deleteOptionsMenuButton != null){
+
+        deleteOptionsMenuButton.addEventListener('click', () => {
+
+            if(selectedElement != null && isItemSelected){
+                selectedElement.remove();
+                hideOptionsMenu();
+            }
+
+        });
+
+    }
+
+    if(addOptionsMenuButton != null){
+
+        addOptionsMenuButton.addEventListener('click', () => {
+
+            if(selectedElement != null && isItemSelected){
+                const element = document.createElement('p');
+                element.contentEditable = true;
+
+                addStopPropagationListener(element)
+                selectableElements.push(element);
+                selectedElement.appendChild(element);
+
+                selectEditableElement(element);
+
+                hideOptionsMenu();
+            }
+
+        });
+
+    }
+
+
+    document.addEventListener('focusin', (e) => {
+        const editable = e.target.closest('[contenteditable]');
+        if (!editable) return;
+        hideAddOptionsButton();
+        showDeleteOptionsButton();
+        selectEditableElement(editable);
+    });
+
+    document.addEventListener('click', (e) => {
+
+        //if clicks away from selectable element or menu
+        if (!e.target.closest('[contenteditable]') && !e.target.closest('#floating_options_menu')) {
+
+            isItemSelected = false;
+
+            if(selectedElement != undefined){
+                selectedElement.classList.remove('selectedElement');  
+            }
+
+            hideOptionsMenu();
+        }
+    });
+
+    document.addEventListener('input', e => {
+        const el = e.target;
+        if (!el.isContentEditable) return;
+
+        if (el.textContent.trim() === '') {
+            el.innerHTML = '';
+        }
+    });
+
+    selectableElementGroups.forEach((element) => {
+
+        element.addEventListener('click', (e) => {
+            showAddOptionsButton();
+            hideDeleteOptionsButton();
+            e.stopPropagation();
+            const title = e.target;
+            const groupElement = title.parentNode;
+            selectEditableElement(groupElement);
+        });
+
+    });
+
+    
+    selectableElements.forEach((element) => {
+
+        addStopPropagationListener(element);
+
+    });
+
 }
 
+function addStopPropagationListener(element){
+
+    element.addEventListener('click', (e) => {
+        e.stopPropagation();
+    });
+
+}
+
+function showAddOptionsButton(){
+    addOptionsMenuButton.classList.remove('hidden');
+}
+
+function hideAddOptionsButton(){
+    addOptionsMenuButton.classList.add('hidden');
+}
+
+function showDeleteOptionsButton(){
+    deleteOptionsMenuButton.classList.remove('hidden');
+}
+
+function hideDeleteOptionsButton(){
+    deleteOptionsMenuButton.classList.add('hidden');
+}
+
+function hideOptionsMenu(){
+    menu.style.zIndex = '-1';
+}
+
+function showOptionsMenu(){
+    menu.style.zIndex = '1000';
+}
+
+function selectEditableElement(element){
+
+    console.log(element);
+
+    if(element == null){
+        return;
+    }
+
+    if(selectedElement != null){
+        selectedElement.classList.remove('selectedElement');
+    }
+
+    selectedElement = element;
+    isItemSelected = true;
+
+    element.classList.add('selectedElement');
+
+    const rect = element.getBoundingClientRect();
+
+    const menuDimensions = menu.getBoundingClientRect();
+    const menuHeight = menuDimensions.height;
+    const menuWidth = menuDimensions.width;
+
+    console.log(menuHeight);
+
+    menu.style.top = `${window.scrollY + rect.top - menuHeight - 5}px`;
+    menu.style.left = `${window.scrollX + rect.right - menuWidth}px`;
+
+    showOptionsMenu();
+
+}
 
 function getInvoiceHTMLString(){
 
-    const htmlString = invoiceBoilerPlate;
+    const htmlString = invoiceBoilerPlateTop + invoiceOrdersHTML + invoiceBoilerPlateBottom;
 
     return htmlString;
 
 }
 
 function downloadPDF() {
+    
     const element = document.getElementById('invoice_preview');
 
     const opt = {
         margin:       15,
         filename:     'invoice.pdf',
         image:        { type: 'jpeg', quality: 1 },
-        html2canvas:  { scale: 2, dpi: 300 },   // Higher = better quality
+        html2canvas:  { scale: 5, dpi: 300 },   // Higher = better quality
         jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' },
         pagebreak: {
             mode: ['css', 'legacy']
@@ -204,7 +365,7 @@ const invoiceStyles =
 'margin-bottom: 20px;'+
 '}'
 
-const invoiceBoilerPlate = 
+const invoiceBoilerPlateTop = 
 '<html lang="en">'+
 '<head>'+
 '<meta charset="UTF-8">'+
@@ -273,7 +434,9 @@ const invoiceBoilerPlate =
 '<th>Unit Price</th>'+
 '<th>Total Price</th>'+
 
-'</tr>'+
+'</tr>'
+
+const invoiceOrdersHTML = 
 
 '<tr class="dataRow">'+
 
@@ -318,7 +481,9 @@ const invoiceBoilerPlate =
 '<td><hr></td>'+
 '</tr>'+
 
-'<tr>'+
+'<tr>';
+
+const invoiceBoilerPlateBottom =
 
 '<td>'+
 '<div>'+
