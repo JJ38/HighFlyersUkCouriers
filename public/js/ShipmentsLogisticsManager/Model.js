f9aa02c41d56b73db67c6ad747c16027c4221c4f
James <jamesbrass@ymail.com>
2026-08-16 18:52:55 +0100

    Change email to monday 5pm. Fixed timezone bug with arrival times

--- public/js/ShipmentsLogisticsManager/Model.js
+++ public/js/ShipmentsLogisticsManager/Model.js
@@ -2348,7 +2348,7 @@ function getStopArrivalTime(optimisedStop, startTimeOffset, additionalDriveTime)
 
   const durationSecondsInt = parseInt(durationSecondsString.replaceAll("s", ""));
 
-  const startTimeString = startTimeDate.substring(startTimeDate.indexOf("T") + 1, startTimeDate.length).replaceAll(["Z"], "");
+  const startTimeString = DateTime.fromISO(startTimeDate, { zone: "utc" }).setZone("Europe/London").toFormat("HH:mm:ss");
   
   const startTimeComponentsStrings = startTimeString.split(":");
 
